20729f272e79b134fe1572eae17552177d64698c
CalmTechie <calmtechie@mail.ru>
2021-03-28 01:48:12 +0300

    i3 config added, removed nvim config

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-mkdir -p "$HOME/.config/nvim"
-mkdir -p "$HOME/.config/nvim/undo"
-ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
-
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
