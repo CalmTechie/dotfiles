5abda77a3d74a718bda341de8749aee8b179ad27
CalmTechie <calmtechie@mail.ru>
2021-08-15 22:20:59 +0300

    added dunst dwm

--- install.sh
+++ install.sh
@@ -12,5 +12,7 @@ ln -sf "$DOTFILES/zsh/external" "$HOME/.config/zsh"
 mkdir -p "$XDG_DATA_HOME"
 cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 
+ln -sf "$DOTFILES/dwm" "$XDG_DATA_HOME"
+
 mkdir -p "$XDG_CONFIG_HOME/dunst"
 ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc" 
